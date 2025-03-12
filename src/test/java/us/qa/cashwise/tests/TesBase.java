3ec47e0f02421c9b83ea6f0d2bc842040d74e05f
zafar2826138 <2826138@gmail.com>
2025-03-12 12:43:47 -0400

    finished Add Product and Services Automation

--- src/test/java/us/qa/cashwise/tests/TesBase.java
+++ src/test/java/us/qa/cashwise/tests/TesBase.java
@@ -6,15 +6,20 @@ import org.testng.annotations.BeforeMethod;
 import utils.ConfigReader;
 import utils.DriverHelper;
 
+import java.time.Duration;
+
 public class TesBase {
-    WebDriver driver = DriverHelper.getDriver();
+    protected WebDriver driver = DriverHelper.getDriver();
     @BeforeMethod
     public void setup(){
 
+      driver.manage().window().maximize();
+       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get(ConfigReader.ReadProperty("cashwiseURL"));
+
     }
     @AfterMethod
     public void tearDown(){
-        driver.quit();
+    //    driver.quit();
     }
 }
